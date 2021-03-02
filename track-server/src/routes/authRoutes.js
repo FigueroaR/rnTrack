72079628e3d602bc59fade4becf9a39ce6a2d407
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-02 12:20:08 -0500

    Error handling

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -9,9 +9,15 @@ const router = express.Router();
 router.post('/signup', async (req, res) => {
     //req is what we get from the server
     const {email, password} = req.body
-    const user = new User({email, password})
-    await user.save() //async opp 
-    res.send('You made a post req')
+    try {
+        const user = new User({email, password})
+        await user.save() //async opp 
+        res.send('You made a post req')
+    } catch(err) {
+        res.status(422).send(err.message)
+        // http erro code 422 === usuer sent inavlid data
+    }
+    
 })
 
 
