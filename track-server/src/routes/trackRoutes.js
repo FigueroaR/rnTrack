6d3f9678355da8b10cf6dfec2e0aa2704ea3f9f7
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-02 17:49:13 -0500

    Creating tracks

--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -15,4 +15,24 @@ router.get('/tracks', async (req, res) => {
     res.send(tracks)
 })
 
+
+router.post('/tracks', async (req, res) => {
+    const  {name, locations } = req.body;
+
+    if (!name || !locations) {
+        return res.status(422).send({error: "You must provide name and locations"})
+    }
+
+    // is user gives us false/trash info, we can handle error with try {} catch() {}
+
+    try{
+        const track = new Track({name, locations, userId: req.user._id})
+        await track.save();
+        res.send(track)
+    } catch(err) {
+        res.status(422).send({error: "Track's location information not correct"})
+    }
+    
+})
+
 module.exports = router; // hook it up in index.js
