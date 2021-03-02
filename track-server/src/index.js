750d5aef5583b1866ee220dacdc41ac69123ea0f
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-02 11:04:16 -0500

    connecting to MongoDB

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,10 +1,27 @@
+
 const express = require ('express') // we need express module
+const mongoose = require('mongoose') // connecting to MongoDB
+
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:nopass@trackapp.38xut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true, // prevent common error message and warnings on terminal 
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', (err) => {
+    console.error('Error Connecting', err)
+})
 
 app.get('/', (req, res) => {
     // every time someone does a request (rew)
     // tou our root route we send a response (res)
-    res.send('Hi there!')
+    res.send('Hiiii there!')
 })
 
 
