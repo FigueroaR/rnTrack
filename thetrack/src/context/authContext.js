d3ea8b3d9792cc83973769ccfbd257678e51e088
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-03 18:43:02 -0500

    Storing the Token

--- thetrack/src/context/authContext.js
+++ thetrack/src/context/authContext.js
@@ -1,29 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
   switch (action.type) {
-    case 'add_error' :
+    case 'add_error':
         return {...state, errorMessage: action.payload }
+    case 'signup': 
+        return {errorMessage: '', token: action.payload};
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-    return async ({email, password}) => {
+const signup = async (dispatch) => {
+   
         // make api request  so sign up emai land pass
         // if we sign up modify our state, and are authenticated
         // if signup fails, reflect error msg
         try {
             
             const response = await trackerApi.post('/signup', {email, password})
-            console.log(response.data)
+            await AsyncStorage.setItem('token', response.data.token)
+            dispatch({type: 'signup', payload: response.data.token})
+            
         } catch(err) {
             //console.log('Dude error', err.response.data);
             dispatch({ type: 'add_error', payload: "Something went Wrong with Sign up" })
         }
-    }
+    
 }
 
 const signin = (dispatch) => {
@@ -41,5 +46,5 @@ const signout = () => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   {signin, signout, signup},
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
