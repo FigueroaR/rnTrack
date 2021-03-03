a65126f7194dab7b4347352d91bee032cfa346d5
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-03 11:34:04 -0500

    Navigation hookup

--- thetrack/App.js
+++ thetrack/App.js
@@ -1,6 +1,9 @@
-// import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-// import { createStackNavigator } from 'react-navigation-stack';
-// import { createBottomTabNavigator } from 'react-navigation-tabs';
+import React from 'react'
+
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 
 import AccountScreen from './src/screens/AccountScreen'
 import SigninScreen from './src/screens/SigninScreen'
@@ -8,3 +11,23 @@ import SignupScreen from './src/screens/SignupScreen'
 import TrackCreateScreen from './src/screens/TrackCreateScreen'
 import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
+
+
+const switchNavigator = createSwitchNavigator({
+    // link to loginFlow
+    loginFlow:  createStackNavigator({ // this will toggle between:
+        Signup: SignupScreen,
+        Signin: SigninScreen
+    }),
+    mainFlow: createBottomTabNavigator({
+        trackListFlow: createStackNavigator({ // this stack has its own sreens and flow
+            TrackList: TrackListScreen,
+            TrackDetail: TrackDetailScreen
+        }),
+        TrackCreate: TrackCreateScreen,
+        Account: AccountScreen
+    })
+})
+
+//top level switch nav, fir show log in flow, then the main flow later on
+export default createAppContainer(switchNavigator)
