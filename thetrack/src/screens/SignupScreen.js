62f1c02d8ba057ede36fbfecf110730ad72196de
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-03 12:00:08 -0500

    Testing the Navigation Flow

--- thetrack/src/screens/SignupScreen.js
+++ thetrack/src/screens/SignupScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, Button} from 'react-native'
+import TrackDetailScreen from './TrackDetailScreen';
 
+// navigation is our in our porps
+const SignupScreen = ({navigation}) => {
+    return <> 
+        <Text Style={{fontSize: 48}}>signup Screen</Text>
+        <Button 
+            title="Go to Sign In" 
+            onPress={() => navigation.navigate('Signin')}/>
 
-const SignupScreen = () => {
-    return (<Text Style={{fontSize: 48}}>signup Screen</Text>)
+        <Button 
+            title="Main Flow" 
+            onPress={() => navigation.navigate('mainFlow')}/>
+            
+        </>
 }
 
 const styles = StyleSheet.create({
--- thetrack/src/screens/TrackListScreen.js
+++ thetrack/src/screens/TrackListScreen.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, Button} from 'react-native'
 
 
-const TrackListScreen = () => {
-    return (<Text Style={{fontSize: 48}}>track list Screen</Text>)
+const TrackListScreen = ({navigation}) => {
+    return <>
+        <Text Style={{fontSize: 48}}>track list Screen</Text>
+        <Button 
+            title="Go to track detail"
+             onPress={() => navigation.navigate('TrackDetail')} />
+    </>
 }
 
 const styles = StyleSheet.create({
