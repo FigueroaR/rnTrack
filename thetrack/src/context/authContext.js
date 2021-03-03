cf583ac0f62a7695b78fb9e18498c9003c6492c5
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-03 14:46:45 -0500

    what the Context Doing?

--- thetrack/src/context/authContext.js
+++ thetrack/src/context/authContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+    return ({email, password}) => {
+        // make api request  so sign up emai land pass
+
+        // if we sign up modify our state, and are authenticated
+
+        // if signup fails, reflect error msg
+    }
+}
+
+const signin = (dispatch) => {
+    return ({}) => {
+        // try to sign in
+        // succes handle state
+        // if failure show err msg
+    }
+}
+
+const signout = () => {
+    //close session
+}
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  {signin, signout, signup},
   { isSignedIn: false }
 );
