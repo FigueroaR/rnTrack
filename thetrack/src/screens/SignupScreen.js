4be1098a32718adafda78cc2706341970799ce4c
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-08 16:47:15 -0500

    extracting Form logic

--- thetrack/src/screens/SignupScreen.js
+++ thetrack/src/screens/SignupScreen.js
@@ -4,47 +4,32 @@ import { Text, Input, Button} from 'react-native-elements' // pre styled compone
 import Spacer from '../components/Spacer'
 import {Context as AuthContext} from '../context/authContext'
 
+import AuthForm from '../components/AuthForm'
+
 
 // navigation is our in our porps
 const SignupScreen = ({navigation}) => {
     const {state, signup} = useContext(AuthContext)
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    
 
-    console.log(state)
+    //console.log(state)
 
 
-    return (<View style={styles.container}> 
-        <Spacer><Text h3>Sign Up for tracker</Text></Spacer>
-        <Input 
-            label="Email" 
-            value={email} 
-            onChangeText={setEmail}
-            autoCapitalize="none"
-            autoCorrect={false}
-            />
-            
-        <Spacer />
-        <Input 
-            label="Password" 
-            value={password} 
-            onChangeText={setPassword}
-            autoCapitalize="none"
-            autoCorrect={false}
-            secureTextEntry // hides our text
-            />
-        {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-        <Spacer>
-            <Button title="Sign Up" onPress={ () => signup({email, password})} />
-        </Spacer>
+    return (
+    <View style={styles.container}> 
+        <AuthForm
+            headerText="Sign Up for Tracker"
+            errorMessage={state.errorMessage}
+            onSubmit={signup}
+            submitButtonText="Sign Up" />
         <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
             <Spacer>
                 <Text style={styles.link}>Sign In</Text>
             </Spacer>
         </TouchableOpacity>
         
-        </View>)
+    </View>)
 }
 
 SignupScreen.navigationOptions = () => {
