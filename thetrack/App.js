4b57af9f4132f7037e4e2d85bbf008af4b1efc82
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-09 13:54:23 -0500

    Creating a navlink

--- thetrack/App.js
+++ thetrack/App.js
@@ -12,7 +12,7 @@ import TrackCreateScreen from './src/screens/TrackCreateScreen'
 import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
 
-import {Provider as AuthProvider} from  './src/context/authContext'
+import {Provider as AuthProvider} from  './src/context/AuthContext'
 
 import { setNavigator } from './src/navigationRef'
 
--- thetrack/src/screens/SignupScreen.js
+++ thetrack/src/screens/SignupScreen.js
@@ -1,31 +1,27 @@
-// import React, { useContext } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import React, { useContext } from 'react';
+import { View, StyleSheet} from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink'
 
 // {we destruct our props}
 const SignupScreen = ({ navigation }) => {
-  const { state, signup } = useContext(AuthContext);
-
-  return (
-    <View style={styles.container}>
-      <AuthForm
-        headerText="Sign Up for Tracker"
-        errorMessage={state.errorMessage}
-        submitButtonText="Sign Up"
-        onSubmit={signup}
-      />
-      <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-        <Spacer>
-          <Text style={styles.link}>
-            Already have an account? Sign in instead
-          </Text>
-        </Spacer>
-      </TouchableOpacity>
-    </View>
-  );
+    const { state, signup } = useContext(AuthContext);
+  
+    return (
+      <View style={styles.container}>
+        <AuthForm
+          headerText="Sign Up for Tracker"
+          errorMessage={state.errorMessage}
+          submitButtonText="Sign Up"
+          onSubmit={signup}
+        />
+        <NavLink
+          routeName="Signin"
+          text="Already have an account? Sign In!"
+        />
+      </View>
+    );
 };
 
 
@@ -40,9 +36,6 @@ const styles = StyleSheet.create({
         flex: 1, // this will make the view fit entire screen
         justifyContent: 'center', //vert center
         paddingBottom: 200
-    },
-    link : {
-        color: 'blue'
     }
 })
 
