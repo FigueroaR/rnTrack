8a43ad20315fe596297de901451c9a9341aec30e
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-09 18:01:40 -0500

    Requesting Locations Permissions

--- thetrack/src/screens/TrackCreateScreen.js
+++ thetrack/src/screens/TrackCreateScreen.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native'
 import {Text} from 'react-native-elements'
 import Map from '../components/Map'
+import {requestPermissionsAsync} from 'expo-location' // expo-cli install npm
 
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null)
+
+
+    const startWatching = async () => {
+        try {
+          const { granted } = await requestPermissionsAsync();
+          if (!granted) {
+            throw new Error('Location permission not granted');
+          }
+        } catch (e) {
+          setErr(e);
+        }
+    };
+
+    useEffect(() => {
+       startWatching(); 
+    }, [])
+
     return (
     <SafeAreaView forceInser={{ top: 'always'}}>
         <Text h2>Create a Track</Text>
         <Map/>
+        { err ? <Text>Please Enable Location</Text> : null}
     </SafeAreaView>)
 }
 
