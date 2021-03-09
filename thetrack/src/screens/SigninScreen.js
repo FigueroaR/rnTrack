f80a01c596c999be9eb94c74829da45bd11f31eb
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-09 14:08:12 -0500

    Real components reuse!

--- thetrack/src/screens/SigninScreen.js
+++ thetrack/src/screens/SigninScreen.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native'
-
+import AuthForm from '../components/AuthForm'
+import NavLink from  '../components/NavLink'
 
 const SigninScreen = () => {
-    return (<Text Style={{fontSize: 48}}>signin Screen</Text>)
+    return (
+        <View style={styles.container}>
+            <AuthForm
+                headerText="Sign in to your Tracker"
+                errorMessage=""
+                submitButtonText="Sign In"
+                onSubmit={() => {}}
+            />
+            <NavLink
+                routeName="Signup"
+                text="Need an account? Sign Up!"
+            />
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
+SigninScreen.navigationOptions = () => {
+    return {
+      headerShown: false,
+    };
+  };
 
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // this will make the view fit entire screen
+        justifyContent: 'center', //vert center
+        paddingBottom: 200
+    }
 })
 
 
