c4966425d0caf278476aacde7c64d2a53d5609f4
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-03-09 16:28:42 -0500

    Safe area views

--- thetrack/src/screens/AccountScreen.js
+++ thetrack/src/screens/AccountScreen.js
@@ -1,6 +1,7 @@
 
 import React, {useContext} from 'react';
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, SafeAreaView} from 'react-native'
+//import {SafeAreaView} from 'react-navigation'
 import {Button} from 'react-native-elements'
 import Spacer from '../components/Spacer'
 import {Context as AuthContext} from '../context/AuthContext'
@@ -9,13 +10,13 @@ const AccountScreen = () => {
     const {signout } = useContext(AuthContext)
  
     return (
-    <>
+    <SafeAreaView forceInset={{top: 'always'}}>
         <Text Style={{fontSize: 48}}>Account Screen</Text>
         <Spacer>
             <Button title='Sign Out' onPress={signout}/>
         </Spacer>
         
-    </>)
+    </SafeAreaView>)
 }
 
 const styles = StyleSheet.create({
